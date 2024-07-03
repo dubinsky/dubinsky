e3caad6f03f5431e867159933fe4510a1f8f8cf2
Leonid Dubinsky <dub@podval.org>
2024-07-03 13:30:17 -0400

    - sort tags and pages in without case;

--- _plugins/all-tags.rb
+++ _plugins/all-tags.rb
@@ -6,8 +6,8 @@ module Jekyll
        (site.documents + site.pages).each do |p|
          p.data["tags"]&.each { |t| hash[t] << p }
        end
-       hash.each_value { |pages| pages.sort { |a,b| a.data["title"] <=> b.data["title"] }.reverse! }
-       hash
+       hash.each_value { |pages| pages.sort_by! { |page| page.data["title"].downcase } }
+       hash.sort_by { |word| word[0].downcase }
      end
     end
   end
